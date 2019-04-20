723ee53690536d78b99c776f58a73bb2a3655019
koki <kkttm530@gmail.com>
2019-04-21 02:17:38 +0900

    ドームになることを良しとする評価関数の設計。メインとの統合。

--- GA/Evaluation.py
+++ GA/Evaluation.py
@@ -9,14 +9,16 @@ import Rhino
 import scriptcontext
 
 
-def pop_evaluation(tim_number, instance_pop):
+def pop_evaluation(tim_number, instance_pop, generation_count, generate_range, generation_num):
     '''
     使用する評価関数を埋め込む用の関数
-    :param tim_number:
-    :param instance_pop:
-    :return:
+    :param tim_number: 個体に使用している部材の数
+    :param instance_pop: Generateクラスのインスタンス
+    :param generation_count: 現在の世代数
+    :param generation_num: 全体の個体数
+    :return:　評価値
     '''
-    evaluate_value = pop_height_evaluate(tim_number, instance_pop)
+    evaluate_value = pop_dome_evaluate(tim_number, instance_pop, generation_count, generate_range, generation_num)
     # evaluate_value = partner_num_evaluate(tim_number, instance_pop, 2, 8)
     return evaluate_value
 
@@ -93,3 +95,69 @@ def pop_height_evaluate(tim_number, instance_pop):
     most_height_value = max(tim_height)
 
     return most_height_value
+
+
+def pop_dome_evaluate(tim_number, instance_pop, generation_count, generate_range, generation_num):
+    '''
+
+    :param tim_number: timberの数
+    :param instance_pop:  Generateクラスのインスタンス
+    :param generation_count: 現在の世代。
+    :param generate_range: 個体間の距離。
+    :param generation_num: 全体の個体数。
+    :return: 範囲に入っていない部材の数。
+    '''
+
+    p = {}
+    p[0] = Rhino.Geometry.Point3d(750, 750, 0)
+    p[1] = Rhino.Geometry.Point3d(2250, 750, 0)
+    p[2] = Rhino.Geometry.Point3d(2250, 2250, 0)
+    p[3] = Rhino.Geometry.Point3d(750, 2250, 0)
+
+    p[4] = Rhino.Geometry.Point3d(750, 750, 1500)
+    p[5] = Rhino.Geometry.Point3d(2250, 750, 1500)
+    p[6] = Rhino.Geometry.Point3d(2250, 2250, 1500)
+    p[7] = Rhino.Geometry.Point3d(750, 2250, 1500)
+
+    points_list = []
+    for i in range(8):
+        points_list.append(p[i])
+
+    brep = Rhino.Geometry.Brep.CreateFromBox(points_list)
+
+    copy_from = Rhino.Geometry.Point3d(0, 0, 0)
+    copy_to = Rhino.Geometry.Point3d((generate_range * 2) * instance_pop.pop_index,
+                                     (-generate_range * 2) * (generation_count + 1), 0)
+    vec_move = copy_to - copy_from
+
+    xf = Rhino.Geometry.Transform.Translation(vec_move)
+    brep.Transform(xf)
+
+    if generation_num == generation_count:
+        rs.AddLayer('dome_box')
+        box = scriptcontext.doc.Objects.AddBrep(brep)
+        rs.ObjectLayer(box, 'dome_box')
+
+    evaluate_point = 0
+    for i in range(tim_number):
+        curve = instance_pop.used_list[i].center_line
+
+        curve = rs.coercecurve(curve)
+
+        end = curve.PointAtEnd
+        start = curve.PointAtStart
+
+        rc, mid_parameter = curve.NormalizedLengthParameter(0.5)
+
+        mid = curve.PointAt(mid_parameter)
+
+        end_p_check = brep.IsPointInside(end, Rhino.RhinoMath.SqrtEpsilon, True)
+        start_p_check = brep.IsPointInside(start, Rhino.RhinoMath.SqrtEpsilon, True)
+        mid_p_check = brep.IsPointInside(mid, Rhino.RhinoMath.SqrtEpsilon, True)
+
+        if end_p_check or start_p_check or mid_p_check:
+            pass
+        else:
+            evaluate_point += 1
+
+    return evaluate_point
